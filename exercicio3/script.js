1130b979aa8f18cf7986acd322d3d1fe7110ca32
unknown <cesarrcosta99@gmail.com>
2022-10-27 19:41:52 -0300

    atualização exercicio3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -2,13 +2,13 @@ const melhoresFrutas = ["Maça", "Banana", "Uva"]
 const numeros = [10, 2, 3]
 const politica = ["Lula", "Bolsonaro", 13, 22]
 
-const copiaFrutas = melhoresFrutas.slice()
+const copiaMelhoresFrutas  = melhoresFrutas.slice()
 const copiaNumeros = numeros.slice()
 const copiaPolitica = politica.slice()
 
 //   Adicione um item `number` ao início do primeiro array. Utilize `console.log()` para 
 //        exibir o original e a cópia;
-copiaFrutas.unshift(10)
+copiaMelhoresFrutas.unshift(10)
 console.log("Original", melhoresFrutas)
 console.log("Copia Frutas", copiaFrutas)
 // Remova o último item do segundo array. Utilize `console.log()` para exibir o original 
