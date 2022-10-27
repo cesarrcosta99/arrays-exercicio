a4d0c5ecee9028142888f35f038d0cf7a04d3f8d
unknown <cesarrcosta99@gmail.com>
2022-10-27 16:58:30 -0300

    Exercicio 1 e 2 Feito

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+// O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma 
+// ordem específica;
+// O segundo array deve possuir apenas **strings**;
+//O terceiro array deve possuir **números**, **strings** e **booleanos**.
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+const arrayNumeros = [5, 2, 1, 4]
+const arrayString = ["César", "Julio", "Breno"]
+const arrayMisturado = [2, "Banana", true]
+console.log(arrayNumeros)
+console.log(arrayString)
+console.log(arrayMisturado)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,19 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ____Confiante____.      ";
+const arrayNumeros = [5, 2, 1, 4]
+const arrayString = ["César", "Julio", "Breno"]
+const arrayMisturado = [2, "Banana", true]
+// Imprima a quantidade de itens de cada array (utilize um console.log() para cada 
+// impressão)
+console.log(arrayNumeros.length)
+console.log(arrayString.length)
+console.log(arrayMisturado.length)
+// Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o 
+// terceiro item do terceiro array
+console.log("1 item",arrayNumeros[0])
+console.log("2 item",arrayString[1])
+console.log("3 item",arrayMisturado[2])
+// Verifique se um item está incluído no primeiro array e depois no terceiro array e 
+//   imprima o resultado destas verificações no console.log(). A primeira impressão deve 
+//        retornar um booleano true e a segunda deve retornar um booleano false.
+console.log(arrayNumeros.includes(2))
+console.log(arrayString.includes("Lucas"))
